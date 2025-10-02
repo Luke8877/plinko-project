64ff0b13d974ad85f36137cd62a4dde70b557e6e
Luke Thompson <luke.thompson996@gmail.com>
2025-10-02 15:32:40 -0600

    Added an authenticatino placeholder component

---
+++ src/components/LoginPlaceholder.jsx
@@ -0,0 +1,8 @@
+export default function LoginPlaceholder() {
+  return (
+    <div>
+      <h2>Authentication System</h2>
+      <p>Users will register, login, and have accounts secured with hashed passwords.</p>
+    </div>
+  );
+}
